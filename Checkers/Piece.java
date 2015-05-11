eee0886996d7bbb98ff4a0644b37a8b3fde207af
pjforelli <pjforelli@mxschool.edu>
2015-05-11 14:12:33 -0400

    Removed useless code

--- Checkers/Piece.java
+++ Checkers/Piece.java
@@ -7,27 +7,5 @@
  */
 public class Piece
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Piece
-     */
-    public Piece()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
