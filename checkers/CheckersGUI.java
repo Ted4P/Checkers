48cd2f16bc5a79a609d98d899a15286b5068075f
Ted Pyne <ted.pyne.4@gmail.com>
2015-05-16 07:43:06 -0400

    Invalid move choices immediately reset turn counter
    Which is way more intutive

--- checkers/CheckersGUI.java
+++ checkers/CheckersGUI.java
@@ -18,7 +18,7 @@ public class CheckersGUI extends javax.swing.JFrame  {
     private JLabel[][] GUIboard;
 
     private JPanel boardGUI;
-    
+
     private boolean selected = false;
 
     private int[][] currentSelected;
@@ -41,69 +41,61 @@ public class CheckersGUI extends javax.swing.JFrame  {
             {
                 //if(board.getPiece(i,j) != null)
                 GUIboard[i][j] = new JLabel();
-                
+
             }
         }
 
-        
         currentSelected = new int[2][2];
-
         boardGUI = new JPanel();
         boardGUI.setLayout(new GridLayout(8,8));
         boardGUI.addMouseListener(new MouseAdapter() {
-            
-                        int selected =0;
-                        
-                        public void mouseClicked(MouseEvent e) {
-                            
-                            if (selected==0)
-                            {
-                                currentSelected[0]=arrayCoord(pressed(e));
-                                selected++;
-                            }
-                            else if (selected ==1)
-                            {
-                                currentSelected[1]=arrayCoord(pressed(e));
-                                move(currentSelected);
-                                    System.out.println("MOVE");
-                                    board.printArr();
-                                
-                                renderBoard();
-                                    System.out.println("RENDER BOARD");
-                                
-                                selected++;
-                                
-                            }
-                            else //initialize, might need to move it into elseif statement
-                            {
-                                currentSelected = new int[2][2];
-                                selected=0;
-                            }
-                            
-                            
-                            
-                                //debugging
-                                
-                            for (int[] curr: currentSelected)
-                            {
-                                for (int j: curr)
-                                    System.out.print(j+" ");
-                                System.out.println();
-                            }
-                            System.out.println();
 
+                int selected =0;
+
+                public void mouseClicked(MouseEvent e) {
+
+                    if (selected==0)
+                    {
+                        currentSelected[0]=arrayCoord(pressed(e));
+                        selected++;
+                        if(!board.isValidSelection(currentSelected[0][1], currentSelected[0][0])){
+                            System.out.println("INVALID MOVE");
+                            currentSelected = new int[2][2];
+                            selected=0;
                         }
-                        
-                        
-                    });
+                    }
+                    else if (selected ==1)
+                    {
+                        currentSelected[1]=arrayCoord(pressed(e));
+                        move(currentSelected);
+                        System.out.println("MOVE");
+                        board.printArr();
 
-        
+                        renderBoard();
+                        System.out.println("RENDER BOARD");
+
+                        currentSelected = new int[2][2];
+                        selected=0;
+                    }
+
+                    //debugging
+
+                    for (int[] curr: currentSelected)
+                    {
+                        for (int j: curr)
+                            System.out.print(j+" ");
+                        System.out.println();
+                    }
+                    System.out.println();
+
+                }
+
+            });
         //do this last
         renderBoard();
 
     }
-    
-    
+
     public void renderBoard()
     {
 
@@ -155,7 +147,6 @@ public class CheckersGUI extends javax.swing.JFrame  {
 
         //boardGUI.addMouseListener(this);
 
-
         pack();
         this.setVisible(true);
 
@@ -165,45 +156,33 @@ public class CheckersGUI extends javax.swing.JFrame  {
     {
 
         Component c = boardGUI.findComponentAt(e.getX(), e.getY());
-        
+
         int[] coordinates = new int[2]; //[x,y]
-        
-                System.out.println(e.getX() + "," + e.getY());
-        
+
+        System.out.println(e.getX() + "," + e.getY());
+
         coordinates[0] = e.getX();
         coordinates[1] = e.getY();
-
-        
-        
-        
-        return coordinates;
-        
-        
-
-           
+        return coordinates; 
     }
-    
-    
+
     private int[] arrayCoord(int[] pixelCoord)
     {
         int[] newArr = new int[2];
-        
+
         for (int i=0; i<2; i++)
             newArr[i] = pixelCoord[i]/MULTIPLIER;
-                
+
         return newArr;
     }
-        
+
     private void move(int[][] currentSelected)
     {
         board.makeMove(currentSelected[0][1],currentSelected[0][0],currentSelected[1][1],currentSelected[1][0]);
-        
+
         System.out.println(currentSelected[0][0]+","+currentSelected[0][1] + " to " + currentSelected[1][0] + "," + currentSelected[1][1]);
     }
-    
-    
-    
-    
+
     public static void tester (String[] args)
     {
         CheckersGUI gui = new CheckersGUI();
@@ -215,6 +194,4 @@ public class CheckersGUI extends javax.swing.JFrame  {
 
     }
 
-    
-    
 }
