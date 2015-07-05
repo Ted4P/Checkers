5f251dccf25cfda32a55d0c3c02f66d240483397
Ted Pyne <ted.pyne.4@gmail.com>
2015-07-05 13:41:03 -0400

    Tested all AI agression settings, determined 2 is most optimal, followed by 1.75 (for 13 move simulatiion w/ 5 move lookahead)

--- Checkers/AITester.java
+++ Checkers/AITester.java
@@ -1,30 +1,41 @@
-import java.util.Scanner;
-
 
 public class AITester {
+	
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
+		//Linked lists, screw OO
+		double[] settings = {.25,.5,.75,1,1.25,1.5,1.75,2};
+		int[] wins = new int[settings.length];
+		
+		int l = settings.length;
+		for(int i = 0 ; i < l; i ++){
+			for(int j = i + 1; j < l; j++){
+				double result[] = testAI(i,j,13);
+				if(result[0]> result[1]) wins[i]++;
+				else if(result[1]>result[0]) wins[j]++;
+			}
+		}
+		for(int r = 0; r < l; r++){
+			System.out.println("AI aggr " + settings[r] + " won " + wins[r] + " games.");
+		}
+	}
+	
+	private static double[] testAI(int AI1, int AI2, int moves){
 		Board board = new Board();
-		
-		System.out.println("Agression of first AI:");
 		AI3 ai1 = new AI3(board, true);
-		ai1.setAggression(scan.nextDouble());
-		
-		System.out.println("Agression of second AI:");
+		ai1.setAggression(AI1);
 		AI3 ai2 = new AI3(board, false);
-		ai2.setAggression(scan.nextDouble());
-		
-		System.out.println("Number of turns:");
-		int i = scan.nextInt();
+		ai2.setAggression(AI2);
+		AI3.setRecur(5);
 		
 		AI3.setRecur(5);
-		while(i>0){
+		while(moves>0){
 			while(ai1.makeMove());
 			while(ai2.makeMove());
-			i--;
+			moves--;
 		}
-		System.out.println("The score for ai1 is: " + board.getBoardScore(true, 1));
-		System.out.println("The score for ai2 is: "+ board.getBoardScore(false, 1));
-		System.out.println(board.toString());
+		double[] score = new double[2];
+		score[0] = -board.getBoardScore(false, 0);
+		score[1] = -board.getBoardScore(true, 0);
+		return score;
 	}
 }
